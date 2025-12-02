144205c25ef8cdbeb959aef087e0744054efc710
Jonny Muir <jonnymoo@hotmail.com>
2025-12-02 08:46:51 +0000

    Add initial Program.cs with main entry point

---
+++ src/ProcessSandbox.Worker/Program.cs
@@ -0,0 +1,16 @@
+namespace ProcessSandbox.Worker;
+
+
+using System;
+
+/// <summary>
+/// 
+/// </summary>
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Entry point for your application
+        Console.WriteLine("Hello from .NET");
+    }
+}
