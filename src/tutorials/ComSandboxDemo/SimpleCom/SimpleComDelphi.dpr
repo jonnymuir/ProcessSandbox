a88725fb9838c18a75180d66f6946354149cb98a
Jonny Muir <jonnypmuir@gmail.com>
2025-12-22 13:20:14 +0000

    refactor: Simplify SimpleCalculator implementation and remove unused factory code

--- src/tutorials/ComSandboxDemo/SimpleCom/SimpleComDelphi.dpr
+++ src/tutorials/ComSandboxDemo/SimpleCom/SimpleComDelphi.dpr
@@ -1,35 +1,26 @@
-library SimpleComDelphi;
+unit SimpleCalculatorImpl;
+
+{$MODE DELPHI}
+
+interface
 
 uses
-  Windows, Activex, ComServ;
-
-const
-  CLSID_SimpleCalculator: TGUID = '{11111111-2222-3333-4444-555555555555}';
-  IID_ICalculator: TGUID = '{E1234567-ABCD-1234-EF12-0123456789AB}';
+  Windows, ComObj, ActiveX, ComServ;
 
 type
-  // The interface definition (Order must match C# VTable)
   ICalculator = interface(IUnknown)
     ['{E1234567-ABCD-1234-EF12-0123456789AB}']
     function Add(a, b: Integer): Integer; stdcall;
-    function GetInfo: WideString; stdcall;
+    function GetInfo: WideString; stdcall; // Matches C# [PreserveSig]
   end;
 
-  // The Implementation Class
-  TSimpleCalculator = class(TInterfacedObject, ICalculator)
-  public
+  TSimpleCalculator = class(TComObject, ICalculator)
+  protected
     function Add(a, b: Integer): Integer; stdcall;
     function GetInfo: WideString; stdcall;
   end;
 
-  // The Class Factory
-  TSimpleCalculatorFactory = class(TInterfacedObject, IClassFactory)
-  public
-    function CreateInstance(const unkOuter: IUnknown; const iid: TGUID; out obj): HResult; stdcall;
-    function LockServer(fLock: BOOL): HResult; stdcall;
-  end;
-
-{ TSimpleCalculator }
+implementation
 
 function TSimpleCalculator.Add(a, b: Integer): Integer; stdcall;
 begin
@@ -38,53 +29,13 @@ end;
 
 function TSimpleCalculator.GetInfo: WideString; stdcall;
 begin
+  // Delphi's WideString is a BSTR. 
+  // The caller (C#) will take ownership and free it.
   Result := 'Running the native Delphi (FPC) COM object';
 end;
 
-{ TSimpleCalculatorFactory }
-
-function TSimpleCalculatorFactory.CreateInstance(const unkOuter: IUnknown; const iid: TGUID; out obj): HResult; stdcall;
-var
-  Calc: TSimpleCalculator;
-begin
-  if unkOuter <> nil then
-  begin
-    Result := CLASS_E_NOAGGREGATION;
-    Exit;
-  end;
-  
-  Calc := TSimpleCalculator.Create;
-  Result := Calc.QueryInterface(iid, obj);
-end;
-
-function TSimpleCalculatorFactory.LockServer(fLock: BOOL): HResult; stdcall;
-begin
-  Result := S_OK;
-end;
-
-// --- DLL Exports ---
-
-function DllGetClassObject(const clsid, iid: TGUID; out obj): HResult; stdcall;
-var
-  Factory: TSimpleCalculatorFactory;
-begin
-  if IsEqualGUID(clsid, CLSID_SimpleCalculator) then
-  begin
-    Factory := TSimpleCalculatorFactory.Create;
-    Result := Factory.QueryInterface(iid, obj);
-  end
-  else
-    Result := CLASS_E_CLASSNOTAVAILABLE;
-end;
-
-function DllCanUnloadNow: HResult; stdcall;
-begin
-  Result := S_FALSE;
-end;
-
-exports
-  DllGetClassObject,
-  DllCanUnloadNow;
-
-begin
+initialization
+  TComObjectFactory.Create(ComServer, TSimpleCalculator, 
+    '{11111111-2222-3333-4444-555555555555}',
+    'SimpleCalculator', '', ciMultiInstance, tmBoth);
 end.
