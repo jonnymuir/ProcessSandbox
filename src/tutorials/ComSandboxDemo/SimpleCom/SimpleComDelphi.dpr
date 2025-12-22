da87a30a2f512d58e2cf3d878d63b2a0e4a1a1f9
Jonny Muir <jonnypmuir@gmail.com>
2025-12-22 13:53:47 +0000

    refactor: Simplify SimpleComDelphi by consolidating interface definitions and improving structure

--- src/tutorials/ComSandboxDemo/SimpleCom/SimpleComDelphi.dpr
+++ src/tutorials/ComSandboxDemo/SimpleCom/SimpleComDelphi.dpr
@@ -3,25 +3,25 @@ library SimpleComDelphi;
 {$MODE DELPHI}
 
 uses
-  Windows, 
-  ComObj, 
-  ActiveX, 
-  ComServ; // This unit implements DllGetClassObject
+  Windows, ActiveX;
 
 const
   CLSID_SimpleCalculator: TGUID = '{11111111-2222-3333-4444-555555555555}';
+  IID_ICalculator: TGUID        = '{E1234567-ABCD-1234-EF12-0123456789AB}';
 
 type
-  ICalculator = interface(IUnknown)
-    ['{E1234567-ABCD-1234-EF12-0123456789AB}']
-    function Add(a, b: Integer): Integer; stdcall;
-    function GetInfo: WideString; stdcall; 
-  end;
-
-  TSimpleCalculator = class(TComObject, ICalculator)
-  protected
+  { The Calculator Implementation }
+  TSimpleCalculator = class(TInterfacedObject, IUnknown)
+  public
     function Add(a, b: Integer): Integer; stdcall;
     function GetInfo: WideString; stdcall;
+  end;
+
+  { The Class Factory Implementation }
+  TSimpleClassFactory = class(TInterfacedObject, IClassFactory)
+  public
+    function CreateInstance(const unkOuter: IUnknown; const iid: TGUID; out obj): HResult; stdcall;
+    function LockServer(fLock: BOOL): HResult; stdcall;
   end;
 
 { TSimpleCalculator }
@@ -33,26 +33,49 @@ end;
 
 function TSimpleCalculator.GetInfo: WideString; stdcall;
 begin
-  Result := 'Running the native Delphi (FPC) COM object';
+  Result := 'Running the manual Delphi FPC COM object';
 end;
 
-// These are the 4 standard COM entry points. 
-// ComServ provides the implementation for these.
+{ TSimpleClassFactory }
+
+function TSimpleClassFactory.CreateInstance(const unkOuter: IUnknown; const iid: TGUID; out obj): HResult; stdcall;
+var
+  Calc: TSimpleCalculator;
+begin
+  if unkOuter <> nil then Exit(CLASS_E_NOAGGREGATION);
+  Calc := TSimpleCalculator.Create;
+  Result := Calc.QueryInterface(iid, obj);
+end;
+
+function TSimpleClassFactory.LockServer(fLock: BOOL): HResult; stdcall;
+begin
+  Result := S_OK;
+end;
+
+{ DLL Exports }
+
+function DllGetClassObject(const clsid, iid: TGUID; out obj): HResult; stdcall;
+var
+  Factory: TSimpleClassFactory;
+begin
+  if IsEqualGUID(clsid, CLSID_SimpleCalculator) then
+  begin
+    Factory := TSimpleClassFactory.Create;
+    // We must QueryInterface the factory to the requested IID (usually IClassFactory)
+    Result := Factory.QueryInterface(iid, obj);
+  end
+  else
+    Result := CLASS_E_CLASSNOTAVAILABLE;
+end;
+
+function DllCanUnloadNow: HResult; stdcall;
+begin
+  Result := S_FALSE;
+end;
+
 exports
   DllGetClassObject,
-  DllCanUnloadNow,
-  DllRegisterServer,
-  DllUnregisterServer;
+  DllCanUnloadNow;
 
 begin
-  // Create the factory inside the begin-end block or initialization
-  TComObjectFactory.Create(
-    ComServer, 
-    TSimpleCalculator, 
-    CLSID_SimpleCalculator, 
-    'SimpleCalculator', 
-    'Simple Calculator COM Object',
-    ciMultiInstance,
-    tmBoth
-  );
 end.
