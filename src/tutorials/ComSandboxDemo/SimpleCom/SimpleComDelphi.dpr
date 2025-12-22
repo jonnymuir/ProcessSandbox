3053176435f2818c6e50fddc677dacc08258bd03
Jonny Muir <jonnypmuir@gmail.com>
2025-12-22 13:24:39 +0000

    refactor: Update SimpleComDelphi to improve interface definition and implementation clarity

--- src/tutorials/ComSandboxDemo/SimpleCom/SimpleComDelphi.dpr
+++ src/tutorials/ComSandboxDemo/SimpleCom/SimpleComDelphi.dpr
@@ -1,26 +1,26 @@
-unit SimpleCalculatorImpl;
+library SimpleComDelphi; // Must match filename SimpleComDelphi.dpr
 
 {$MODE DELPHI}
-
-interface
 
 uses
   Windows, ComObj, ActiveX, ComServ;
 
 type
+  // 1. Interface Definition
   ICalculator = interface(IUnknown)
     ['{E1234567-ABCD-1234-EF12-0123456789AB}']
     function Add(a, b: Integer): Integer; stdcall;
-    function GetInfo: WideString; stdcall; // Matches C# [PreserveSig]
+    function GetInfo: WideString; stdcall; 
   end;
 
+  // 2. Implementation Class
   TSimpleCalculator = class(TComObject, ICalculator)
   protected
     function Add(a, b: Integer): Integer; stdcall;
     function GetInfo: WideString; stdcall;
   end;
 
-implementation
+{ TSimpleCalculator Implementation }
 
 function TSimpleCalculator.Add(a, b: Integer): Integer; stdcall;
 begin
@@ -29,11 +29,11 @@ end;
 
 function TSimpleCalculator.GetInfo: WideString; stdcall;
 begin
-  // Delphi's WideString is a BSTR. 
-  // The caller (C#) will take ownership and free it.
+  // This allocates a BSTR that .NET will free
   Result := 'Running the native Delphi (FPC) COM object';
 end;
 
+// 3. The Factory Registration
 initialization
   TComObjectFactory.Create(ComServer, TSimpleCalculator, 
     '{11111111-2222-3333-4444-555555555555}',
